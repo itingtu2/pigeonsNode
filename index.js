e2a6c9be9ac9d73e1919e5d7355a293e74192acc
jennyho2 <jennyho2@illinois.edu>
2017-02-25 16:12:40 -0600

    making dropdown menus

--- index.js
+++ index.js
@@ -17,27 +17,3 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-var conn;
-var pg = require('pg');
-pg.default.ssl = true;
-var str = "postgres://ywnodfgeebqfxe:ad885309fb8aac545475b7de44d1b1bb2e8421a7aac7ebae3c18316b8ab90111@ec2-23-21-204-166.compute-1.amazonaws.com:5432/d84j9nfcrovrde"
-var connectionString = "pg://ywnodfgeebqfxe:ad885309fb8aac545475b7de44d1b1bb2e8421a7aac7ebae3c18316b8ab90111@ec2-23-21-204-166.compute-1.amazonaws.com/d84j9nfcrovrde"
-pg.connect(connectionString, function(err,client,done){
-	if(err){
-		console.log(err);
-	}
-	conn = client;
-})
-
-app.use(function(req, res, next) {
- res.header("Access-Control-Allow-Origin", "*");
- res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
- next();
-});
-
-app.post('/insertProj', function(req,res){
-	console.log("working")
-	var body = req.body
-	console.log("Body: ", body)
-	var stmt =
-})
